bcf6aa98a35fd70ede8451b00aece2ee4314db9a
csillag <csillag@harmacolindor.com>
2017-03-28 17:10:10 +0200

    Switch to rc7, engage FragmentMatcher

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { IntrospectionFragmentMatcher } from 'apollo-client';
+import { IntrospectionFragmentMatcher } from 'apollo-client';
 import { ApolloClient, ApolloProvider } from 'react-apollo';
 import { networkInterface } from './graphql/networkInterface';
 import App from './App';
 
 const client = window.apollo = new ApolloClient({
     networkInterface,
-//    fragmentMatcher: new IntrospectionFragmentMatcher(),
+    fragmentMatcher: new IntrospectionFragmentMatcher(),
 });
 
 ReactDOM.render(
